861f22d4b1fd2d88422cdd110b672166fa6b8484
ciroribba <ciro.ribba@gmail.com>
2025-09-21 17:52:44 -0300

    add file environments

--- src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { RouterLink, RouterLinkActive } from '@angular/router';
 interface MenuOption {
   label: string;
   sublabel: string;
@@ -9,7 +9,7 @@ interface MenuOption {
 
 @Component({
   selector: 'gifs-side-menu-options',
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './side-menu-options.component.html',
 })
 export class SideMenuOptionsComponent {
